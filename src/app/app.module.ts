77661fdfb918e414465f8e565be82b6a343baf34
Rodrigo Aguilar <u202210584@upc.edu.pe>
2024-04-24 23:50:03 -0500

    feat(subscriptions): implements tariff card.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscriptionsCardComponent } from './shared/components/subscriptions-card/subscriptions-card.component';
+import {NgOptimizedImage} from "@angular/common";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatToolbar} from "@angular/material/toolbar";
+import { ImageCardComponent } from './shared/components/image-card/image-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscriptionsCardComponent,
+    ImageCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatCardTitle,
+    MatToolbar
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/components/subscriptions-card/subscriptions-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriptions-card',
+  templateUrl: './subscriptions-card.component.html',
+  styleUrl: './subscriptions-card.component.css'
+})
+export class SubscriptionsCardComponent {
+
+}
