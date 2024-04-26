405bc564d1ff86d11a11f8bbbc8a7754a63c942c
Augusto Pin <augustopm607@gmail.com>
2024-04-26 11:45:18 -0500

    feat(feature/social-interaction): implements star rating, contact card and description card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import { StarRatingComponent} from "./public/components/star-rating/star-rating.component";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatCardModule} from "@angular/material/card";
+import { ContactCardComponent } from './public/components/contact-card/contact-card.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { DescriptionCardComponent } from './public/components/description-card/description-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarRatingComponent,
+    ContactCardComponent,
+    DescriptionCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/public/components/description-card/description-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-description-card',
+  templateUrl: './description-card.component.html',
+  styleUrl: './description-card.component.css'
+})
+export class DescriptionCardComponent {
+
+}
