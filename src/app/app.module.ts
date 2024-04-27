0a77a9745de997d01e6d588499d1a31ab82bcc8f
Kuni_2005 <math.kuni123@gmail.com>
2024-04-26 23:14:20 -0500

    doc: add cards, footer and toolbar sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatMenuModule} from "@angular/material/menu";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {Toolbar} from './public/components/toolbar/toolbar.component';
+import { Footer } from './public/components/footer/footer.component';
+import {FarmCards} from './search/components/farm-cards/farm-cards.component';
+
+let TranslateModule;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Toolbar,
+    Footer,
+    FarmCards,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    NgOptimizedImage,
+    MatToolbar,
   ],
   providers: [],
   bootstrap: [AppComponent]
