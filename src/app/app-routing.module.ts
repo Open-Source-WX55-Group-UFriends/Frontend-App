4a16950005257d0643a9019928f5ca01028860b2
Jean Franco <u202219284@upc.edu.com>
2024-05-29 23:57:02 -0500

    change(profile-team): updated description shed component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import {FinancialStatsPageComponent} from "./financial-stats/pages/financial-sta
 import {SubscriptionsCardComponent} from "./public/components/subscriptions-card/subscriptions-card.component";
 import {PaymentSubscriptionComponent} from "./register/components/profile-page/payment/payment-subscription.component";
 import {HomeComponent} from "./public/pages/home/home.component";
-import {
-  DescriptionShedComponent
-} from "./shared/components/descriptions-sheds-card/description-shed/description-shed.component";
+import { DescriptionShedComponent } from "./shared/components/descriptions-sheds-card/description-shed/description-shed.component";
 import {TaskTableComponent} from "./task/components/task-table/task-table.component";
 import {TaskFormComponent} from "./task/components/task-form/task-form.component";
 import {RegisterCardComponent} from "./register/components/register-card/register-card.component";
@@ -20,7 +18,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'subscriptions/card', component:SubscriptionsCardComponent },
   {path: 'subscriptions/:card', component:PaymentSubscriptionComponent },
-  {path: 'descriptions/:shed', component:DescriptionShedComponent },
+  { path: 'descriptions/:id', component: DescriptionShedComponent },
   {path: 'home', component:HomeComponent },
   {path: 'tasks', component:TaskTableComponent },
   {path: 'tasks/create', component:TaskFormComponent }  ,
--- src/app/shared/components/descriptions-sheds-card/description-shed/description-shed.component.ts
+++ src/app/shared/components/descriptions-sheds-card/description-shed/description-shed.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FarmService } from '../../../../profile-farm/farm/farm.service';
 
 @Component({
   selector: 'app-description-shed',
   templateUrl: './description-shed.component.html',
-  styleUrl: './description-shed.component.css'
+  styleUrls: ['./description-shed.component.css']
 })
-export class DescriptionShedComponent {
-  Handle(event:number)
-  {
+export class DescriptionShedComponent implements OnInit {
+  farm: any;
+
+  constructor(private route: ActivatedRoute, private farmService: FarmService) { }
+
+  ngOnInit(): void {
+    this.getFarmData();
+  }
+
+  getFarmData(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.farmService.getFarmById(id).subscribe((data: any) => {
+        this.farm = data;
+        console.log(this.farm);
+      });
+    } else {
+      console.error('No se proporcionó un ID de granja');
+      return;
+    }
+  }
+
+  Handle(event:number) {
     alert(`You rate ${event}`);
   }
 }
