b82cf0d358f78640ad328abad5978a40e12063d6
Jean Franco <u202219284@upc.edu.com>
2024-05-29 23:58:01 -0500

    feat(profile-team): added farm service

--- src/app/profile-farm/farm/farm.service.ts
+++ src/app/profile-farm/farm/farm.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +8,29 @@ export class FarmService {
   private farms: any[] = [];
   private farmsSubject = new BehaviorSubject<any[]>([]);
 
+  constructor() {
+    // Id generator
+    this.farms = this.farms.map((farm, index) => {
+      return { ...farm, id: `farm${index + 1}` };
+    });
+  }
+
   addFarm(farm: any) {
-    console.log(farm); // Añade esta línea
-    this.farms.push(farm);
+    console.log(farm);
+
+    //Add an id for the farm xd
+    const id = `farm${this.farms.length + 1}`;
+    this.farms.push({ ...farm, id });
     this.farmsSubject.next(this.farms);
   }
 
   getFarms() {
     return this.farmsSubject.asObservable();
+  }
+
+  getFarmById(id: string) {
+    const farm = this.farms.find(farm => farm.id === id);
+    console.log('getFarmById called, found farm:', farm);
+    return of(farm);
   }
 }
