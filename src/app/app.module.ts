7c1bdd70f9ce2072590dff320af3599dbc3ff082
Rodrag0lvr <crodrigo7132gmail.com>
2024-06-05 14:07:59 -0500

    feat: desploy web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,6 +74,31 @@ import { AddEmployeeComponent } from './monitoring/add-employee/add-employee.com
 import { EmergencyComponent } from './monitoring/emergency/emergency.component';
 import { DashboardTaskComponent } from './monitoring/dashboard-task/dashboard-task.component';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtUbu84hggmjfW7y35RXgpYqWctnA9EI8",
+  authDomain: "farmlogitech-op.firebaseapp.com",
+  projectId: "farmlogitech-op",
+  storageBucket: "farmlogitech-op.appspot.com",
+  messagingSenderId: "1078971844719",
+  appId: "1:1078971844719:web:88181c0c35993fdd95e59a",
+  measurementId: "G-GKKPSFZQLN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
