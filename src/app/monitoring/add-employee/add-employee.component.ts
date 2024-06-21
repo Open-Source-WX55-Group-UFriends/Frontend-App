17dd1977abff09602a677ca6b54411eaaa8d76aa
LucianRuiz <luciano.software@hotmail.com>
2024-06-21 15:36:29 -0500

    feat: Connecting the feature to the backend (just part of adding employees and displaying them)

--- src/app/monitoring/add-employee/add-employee.component.ts
+++ src/app/monitoring/add-employee/add-employee.component.ts
@@ -1,39 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import{EmployeeService} from "../service/employee.service";
+import { AuthenticationService } from "../../register/services/authentication.service";
+import { EmployeeService } from "../service/employee.service";
+
+interface Employee {
+  name: string;
+  username: string; // Cambiado de email a username
+  phone: string;
+  password: string;
+  position: string;
+}
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
-export class AddEmployeeComponent {
-  employee = {
-    id: '',
+export class AddEmployeeComponent implements OnInit {
+  employee: Employee = {
     name: '',
-    email: '',
-    password: '',
+    username: '', // Cambiado de email a username
     phone: '',
-    position: '',
-
+    password: '',
+    position: ''
   };
+  isSignedIn: boolean = false;
 
-  constructor(private employeeService: EmployeeService, private router: Router) { }
+  constructor(
+    private employeeService: EmployeeService,
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.authService.isSignedIn.subscribe(
+      signedIn => {
+        this.isSignedIn = signedIn;
+        if (!signedIn) {
+          this.router.navigate(['/sign-in']);
+        }
+      }
+    );
+  }
 
   saveEmployee() {
-    this.employeeService.addEmployee(this.employee);
-    this.employee = {
-      id: '',
-      name: '',
-      email: '',
-      password: '',
-      phone: '',
-      position: '',
+    if (!this.isSignedIn) {
+      console.error('User is not signed in');
+      this.router.navigate(['/sign-in']);
+      return;
+    }
 
-    };
-    this.router.navigate(['/employee']);
-  }
-  goBack() {
-    this.router.navigate(['/employee']);
+    this.employeeService.createEmployee(this.employee).subscribe(
+      (response: any) => {
+        console.log('Employee created successfully', response);
+        this.router.navigate(['/employee']);
+      },
+      (error: any) => {
+        console.error('Error creating employee', error);
+        if (error.status === 401) {
+          this.authService.signOut();
+        }
+      }
+    );
   }
 }
