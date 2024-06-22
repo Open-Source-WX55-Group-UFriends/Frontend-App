9e1b5c36f502dfd8a753a0275773999b0361dc37
Augusto Pin <augustopm607@gmail.com>
2024-06-22 01:34:30 -0500

    feat: add animal and add crops

--- src/app/monitoring/pages/add-animals/add-animals.component.ts
+++ src/app/monitoring/pages/add-animals/add-animals.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {environment} from "../../../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthenticationService} from "../../../register/services/authentication.service";
+import {Router} from "@angular/router";
+import {catchError, Observable, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-add-animals',
@@ -6,5 +11,52 @@ import { Component } from '@angular/core';
   styleUrl: './add-animals.component.css'
 })
 export class AddAnimalsComponent {
+  isAnimalCreated = false;
+
+  private baseURL = environment.serverBasePath;
+  animal = {
+    age: '',
+    shed: '',
+    healthCondition: '',
+    location: ''
+  };
+
+  constructor(private http: HttpClient,
+              private authService: AuthenticationService,
+              private router: Router) {  }
+
+  addAnimal(animalRequest: any): Observable<any> {
+    return this.authService.getToken().pipe(
+      switchMap(token => {
+        console.log('Token de autenticación:', token);
+
+        const httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          })
+        };
+
+        return this.http.post<any>(`${this.baseURL}/animal`, animalRequest, httpOptions);
+      }),
+      catchError(error => {
+        console.error('Error al crear el animal:', error);
+        throw error;
+      })
+    );
+  }
+
+  saveAnimal() {
+    console.error('El animal no existe, creando uno nuevo:');
+    this.addAnimal(this.animal).subscribe(createdAnimal => {
+      console.log('Animal creado con éxito:', createdAnimal);
+      this.router.navigate(['/']);
+      this.isAnimalCreated = false;
+    }, error => {
+      console.error('Ocurrió un error al crear el animal:', error);
+      this.router.navigate(['/']);
+
+    });
+  }
 
 }
