d1465e07288ce929cbb8a214f26a27365430fd6c
Rodrag0lvr <crodrigo7132@gmail.com>
2024-06-24 18:00:58 -0500

    feat: Add endpoint db

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   // Server Base Path for Fake REST API
   // serverBasePath: 'http://localhost:3000/api/v1'
   // Server Base Path for Spring Boot REST API
-  serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'http://farmlogitech-aws-env.eba-bumjqkc2.us-east-2.elasticbeanstalk.com/api/v1'
 };
